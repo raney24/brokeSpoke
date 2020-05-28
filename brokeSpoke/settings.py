5287b9e14f9153e65f489a50f8fda4ae0a0dc3d5
Will Shapiro <willshapiro@will.local>
2020-05-28 16:05:11 -0400

    attempt at deployment

--- brokeSpoke/settings.py
+++ brokeSpoke/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,4 +139,3 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
-django_heroku.settings(locals())
