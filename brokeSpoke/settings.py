fdb2b132a072b15751c58c4a993c15b2130e273e
Will Garman <wgarman702@gmail.com>
2022-09-17 20:04:38 -0400

    remove dotenv

--- brokeSpoke/settings.py
+++ brokeSpoke/settings.py
@@ -15,10 +15,6 @@ import subprocess
 import dj_database_url
 import psycopg2
 
-from dotenv import load_dotenv
-
-load_dotenv('.env') #to fix error not finding .env locally
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
